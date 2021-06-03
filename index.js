b471215729f88531ff78de6b27b1e68a79e76a06
Vrinda Ahuja <vvahuja2000@gmail.com>
2021-06-03 11:06:30 +0530

    Modifiactions

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var data_js = {
 
 const appearOptions = {
     threshold: 0,
-    rootMargin: "0px 0px -200px 0px"
+    rootMargin: "0px 0px -250px 0px"
 };
 
 const appearOnScroll = new IntersectionObserver(
